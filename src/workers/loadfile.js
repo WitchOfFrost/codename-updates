498ab02a5904f1a5b9b9c4cb8a5151718a511b2b
WitchOfFrost <unlimited.gamer@gmx.de>
2021-01-31 14:41:25 +0100

    Added Writefile function to write specific patterns to file

--- src/workers/loadfile.js
+++ src/workers/loadfile.js
@@ -3,14 +3,29 @@ import Logger from "./logger.js";
 
 let configData;
 
-export default class LoadFile {
+export default class ReadFile {
     static cfg() {
         try {
         configData = fs.readFileSync('./src/json/config.json', 'utf-8');
     } catch (e){
         Logger.err(e);
     } finally {
-        Logger.dbg(configData);
+        Logger.dbg(`Read config from file: ${configData}`);
         return(configData);
     }
 }};
+
+export class WriteFile {
+    static cfg(cfg, i) {
+        try {
+            cfg.services[i].updated = Date.now();
+            let configData = JSON.stringify(cfg);
+            fs.writeFileSync('./src/json/config.json', configData, 'utf-8'); 
+        } catch(e) {
+            Logger.err(e);
+        } finally {
+            Logger.dbg(`Wrote config to file: ${configData}`);
+            return("Success");
+        }
+    }
+}
