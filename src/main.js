8f4c35a51be9e8c2a9f52d072885cc4948bae657
WitchOfFrost <unlimited.gamer@gmx.de>
2021-01-31 17:27:55 +0100

    Changed main in favor of services.json

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import ReadFile from "./workers/loadfile.js";
 import Index from "./services/routes.js";
 
 let config;
+let services;
 
 
 function main() {
@@ -11,9 +12,11 @@ function main() {
         Logger.info("Running check()");
 
         config = await JSON.parse(ReadFile.cfg());
+        services = await JSON.parse(ReadFile.services())
+
 
         config.services.forEach(function (service, i) {
-            if (config.supported_services.includes(service.sid)) {
+            if (services.supported_services.includes(service.sid)) {
                 switch (service.sid) {
                     case "reddit":
                         console.log
