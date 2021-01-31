7ba9cf97906268a45f4fb87961249d368322a80c
WitchOfFrost <unlimited.gamer@gmx.de>
2021-01-31 14:40:37 +0100

    Added handler for reddit sid

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
-import LoadFile from "./workers/loadfile.js";
+import Logger from "./workers/logger.js";
+import ReadFile from "./workers/loadfile.js";
+import Index from "./services/routes.js";
 
 let config;
 
 async function main(){
-    config = await JSON.parse(LoadFile.cfg());
+    config = await JSON.parse(ReadFile.cfg());
+
+    config.services.forEach(function(service, i) {
+        if (config.supported_services.includes(service.sid)) {
+            switch (service.sid){
+                case "reddit":
+                console.log
+                Index.Reddit().notif_get(config, service, i);
+                break; 
+            };
+        } else {
+            Logger.warn(`Service ID ${service.sid} is not supported! You can request it on https://github.com/WitchOfFrost/codename-updates !`)
+        };
+    });
 };
 main();
